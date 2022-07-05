74dabf8a5e438bc91cede9ec000e9cb17ed5361d
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-07-05 21:07:52 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,19 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  console.log(RMFunctions.trees);
+  console.log(trees);
+  wildlife();
+  RMFunctions.wildlife();
+  RMFunctions.elevation();
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
